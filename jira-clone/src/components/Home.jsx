51d54c3b5aa88c3e6ee4e8872fd52fb3ded78f66
PatrickZablocki <Patrick Zablocki>
2024-02-15 11:38:32 +0100

    Ich bin fertig und merge gleich

--- jira-clone/src/components/Home.jsx
+++ jira-clone/src/components/Home.jsx
@@ -1,43 +1,70 @@
-import React, { useState } from 'react';
+import{ useState } from 'react';
 import './Home.css';
 
-function Home() {
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+function Home()
 
-    const toggleDropdown = () => {
-        setIsDropdownOpen(!isDropdownOpen);
-    };
+        {
+            const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+            const toggleDropdown = () => {
+                setIsDropdownOpen(!isDropdownOpen);
+            };
 
     return (
         <div className="home-container">
+            {/* Hier beginnt der Header */}
             <header className="header">
                 <div className="header-content">
-                <h1>Jira-Clone</h1>
-                </div>
-                <div className="profile-container">
-                    <a href="#" onClick={toggleDropdown}>
-                    <img className="profile-image" src="/ProfileImage/Profile1.png" alt="Profile" onClick={toggleDropdown} />
-                    </a>
-                    {isDropdownOpen && (
-                        <div className="dropdown-menu">
-                            <ul>
-                                <li>Profil Bearbeiten</li>
-                                <li>Einstellungen</li>
-                                <li>Ausloggen</li>
-                            </ul>
-                        </div>
-                    )}
-                </div>
-            </header>
-            <div className="content">
-                <div className="sidebar">
+                <nav className="navbar">
+                    <div className="header-content">
+                        <a href="/">
+                            <img className="Logo-Image" src="/LogoImage/Logo.png" alt="Logo from Jira" />
+                        </a>
+                    </div>
                     <ul>
                         <li><a href="#">Your Work</a></li>
                         <li><a href="#">Projects</a></li>
                         <li><a href="#">Filters</a></li>
                         <li><a href="#">Dashboard</a></li>
+                        <li><a href="#">People</a></li>
+                        <button className="CreateBtn">Create</button>
                     </ul>
+                </nav>
+                <div className="Search">
+                    <span class="Search-icon">&#128269;</span>
+                    <input className="Search-content" type="search" placeholder="Search"/>        
                 </div>
+                </div>
+                <div className="profile-container">
+                    <button className="settingBtn"><span class="icon">⚙️</span></button>
+                    <button className="LogInBtn"><a href="">Sign in</a></button>
+                    {/* {isDropdownOpen && (
+                        <div className="dropdown-menu">
+                            <ul>
+                                <a className= "dropdown-content" href="">Profil Bearbeiten<li></li></a>
+                                <a className= "dropdown-content" href="">Einstellungen<li></li></a>
+                                <a className= "dropdown-content" href="">Ausloggen<li></li></a>
+                            </ul>
+                        </div>
+                    )} */}
+                </div>
+            </header>
+            {/* Hier Hört der Header auf */}
+            {/* Hier geht die Sidebar los */}
+            <div className="content">
+            <div className="Sidebar">
+                <h2>Planning</h2>
+                <ul>
+                    <li><a href="#"><span class="icon">&#x1F4C5;</span>Timeline</a></li>
+                    <li><a href="#"><span class="icon">&#x1F4A1;</span>Backlog</a></li>
+                    <li><a href="#"><span class="icon">&#x1F5C3;</span>Board</a></li>
+                    <li class="line"></li>
+                    <li><a href="#"><span class="icon">&#x271A;</span>Add shortcut</a></li>
+                    <li><a href="#"><span class="icon">⚙️</span>Project settings</a></li>
+                </ul>
+            </div>
+            {/* HIer endet die Sidebar */}
+            {/* Ab hier beginnt der Main Content */}
                 <div className="main-view">
                     <h2>Backlog</h2>
                     <div className="task">
@@ -50,6 +77,8 @@ function Home() {
                     </div>
                 </div>
             </div>
+            {/* Hier endet der Main content */}
+            {/* Hier beginnt der Footer */}
             <footer className="footer">
                 <p>© {new Date().getFullYear()} Jira-Clone</p>
             </footer>
