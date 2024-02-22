e441e96c07bca5c7fb61ca5dac5861effa6029e6
aselkaldd <kaldarbaeva.asel@gmail.com>
2024-02-22 16:29:39 +0100

    fixed package json

--- jira-clone/src/components/Home.jsx
+++ jira-clone/src/components/Home.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import axios from 'axios';
 import './Home.css';
 import ToDoColumn from './ToDoColumn';
