d7b05ba1de7806a3619735a3e08e52797c6eaaa9
Janis <janismonique92@gmail.com>
2024-02-26 08:51:47 +0100

    part of hamburger menu

--- jira-clone/src/components/Home.jsx
+++ jira-clone/src/components/Home.jsx
@@ -5,118 +5,116 @@ import "./Home.css";
 
 
 function Home() {
-// const [boards,setBoards] = useState([
-//   {
-//     id:Date.now() + Math.random()*2,
-//     title:"To Do",
-//     cards:[
-//       {
-//         id:Date.now() + Math.random(),
-//         title:"Card 1",
-//         tasks:[],
-//         labels:[{
-//           text:"Frontend",
-//           color:"red"
-//         }],
-//         desc:"This is a description",
-//         date:"",
-//       },
-//       {
-//         id:Date.now() + Math.random(),
-//         title:"Card 2",
-//         tasks:[],
-//         labels:[{
-//           text:"Backend",
-//           color:"blue"
-//         }],
-//         desc:"This is a description",
-//         date:"",
-//       }
-//     ]
-//   }
-// ])
+  // const [boards,setBoards] = useState([
+  //   {
+  //     id:Date.now() + Math.random()*2,
+  //     title:"To Do",
+  //     cards:[
+  //       {
+  //         id:Date.now() + Math.random(),
+  //         title:"Card 1",
+  //         tasks:[],
+  //         labels:[{
+  //           text:"Frontend",
+  //           color:"red"
+  //         }],
+  //         desc:"This is a description",
+  //         date:"",
+  //       },
+  //       {
+  //         id:Date.now() + Math.random(),
+  //         title:"Card 2",
+  //         tasks:[],
+  //         labels:[{
+  //           text:"Backend",
+  //           color:"blue"
+  //         }],
+  //         desc:"This is a description",
+  //         date:"",
+  //       }
+  //     ]
+  //   }
+  // ])
 
-// const [target,setTarget]=useState({
-//   cid:"",
-//   bid:"",
-// });
+  // const [target,setTarget]=useState({
+  //   cid:"",
+  //   bid:"",
+  // });
 
-// const addCard=(title,bid)=>{
-//   const card={
-//     id:Date.now() + Math.random(),
-//     title,
-//     labels:[],
-//     tasks:[],
-//     date:"",
-//     desc:"",
-//   };
+  // const addCard=(title,bid)=>{
+  //   const card={
+  //     id:Date.now() + Math.random(),
+  //     title,
+  //     labels:[],
+  //     tasks:[],
+  //     date:"",
+  //     desc:"",
+  //   };
 
-//   const index=boards.findIndex((item)=>item.id===bid);
-//   if(index < 0) return;
+  //   const index=boards.findIndex((item)=>item.id===bid);
+  //   if(index < 0) return;
 
-//   const tempBoards=[...boards]
-//   tempBoards[index].cards.push(card);
-//   setBoards(tempBoards);
-// };
+  //   const tempBoards=[...boards]
+  //   tempBoards[index].cards.push(card);
+  //   setBoards(tempBoards);
+  // };
 
-// const removeCard=(cid,bid)=>{
-//   const bIndex=boards.findIndex((item)=>item.id===bid);
-//   if(bIndex < 0) return;
+  // const removeCard=(cid,bid)=>{
+  //   const bIndex=boards.findIndex((item)=>item.id===bid);
+  //   if(bIndex < 0) return;
 
-//   const cIndex=boards[bIndex].cards.findIndex((item)=>item.id===cid);
-//   if(cIndex < 0) return;
+  //   const cIndex=boards[bIndex].cards.findIndex((item)=>item.id===cid);
+  //   if(cIndex < 0) return;
 
-//   const tempBoards=[...boards];
-//   tempBoards[bIndex].cards.splice(cIndex,1);
-//   setBoards(tempBoards);
-// };
+  //   const tempBoards=[...boards];
+  //   tempBoards[bIndex].cards.splice(cIndex,1);
+  //   setBoards(tempBoards);
+  // };
 
-// const addBoard=(title)=>{
-//   setBoards([...boards,{
-//     id:Date.now()+Math.random(),
-//     title,
-//     cards:[]
-//   }]);
-// };
+  // const addBoard=(title)=>{
+  //   setBoards([...boards,{
+  //     id:Date.now()+Math.random(),
+  //     title,
+  //     cards:[]
+  //   }]);
+  // };
 
-// const removeBoard=bid=>{
-//   const tempBoards=boards.filter(item=>item.id!==bid);
+  // const removeBoard=bid=>{
+  //   const tempBoards=boards.filter(item=>item.id!==bid);
 
-//   setBoards(tempBoards);
-// }
-// const handleDragEnter=(cid,bid)=>{
-// setTarget({
-//   cid,
-//   bid,
-// });
-  
-// };
+  //   setBoards(tempBoards);
+  // }
+  // const handleDragEnter=(cid,bid)=>{
+  // setTarget({
+  //   cid,
+  //   bid,
+  // });
 
-//   const handleDragEnd=(cid,bid)=>{
-//     let s_bIndex,s_cIndex,t_bIndex,t_cIndex;
-    
-//     s_bIndex=boards.findIndex((item)=>item.id===bid);
-//     if(s_bIndex < 0) return;
-    
-//     s_cIndex=boards[s_bIndex].cards.findIndex((item)=>item.id===cid);
-//     if(s_cIndex < 0) return;
-    
-//     t_bIndex=boards.findIndex((item)=>item.id===target.bid);
-//     if(t_bIndex < 0) return;
-    
-//     t_cIndex=boards[t_bIndex].cards.findIndex((item)=>item.id===target.cid);
-//     if(t_cIndex < 0) return;
+  // };
 
-//     const tempBoards=[...boards];
-//     const tempCard=tempBoards[s_bIndex].cards[s_cIndex]
+  //   const handleDragEnd=(cid,bid)=>{
+  //     let s_bIndex,s_cIndex,t_bIndex,t_cIndex;
 
-//     tempBoards[s_bIndex].cards.splice(s_cIndex,1)
-//     tempBoards[t_bIndex].cards.splice(t_cIndex,0,tempCard);
+  //     s_bIndex=boards.findIndex((item)=>item.id===bid);
+  //     if(s_bIndex < 0) return;
 
-//     setBoards(tempBoards);
-// };
+  //     s_cIndex=boards[s_bIndex].cards.findIndex((item)=>item.id===cid);
+  //     if(s_cIndex < 0) return;
 
+  //     t_bIndex=boards.findIndex((item)=>item.id===target.bid);
+  //     if(t_bIndex < 0) return;
 
+  //     t_cIndex=boards[t_bIndex].cards.findIndex((item)=>item.id===target.cid);
+  //     if(t_cIndex < 0) return;
+
+  //     const tempBoards=[...boards];
+  //     const tempCard=tempBoards[s_bIndex].cards[s_cIndex]
+
+  //     tempBoards[s_bIndex].cards.splice(s_cIndex,1)
+  //     tempBoards[t_bIndex].cards.splice(t_cIndex,0,tempCard);
+
+  //     setBoards(tempBoards);
+  // };
 
   // const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
@@ -139,6 +137,7 @@ function Home() {
                 />
               </a>
             </div>
+
             <ul>
               <li>
                 <a href="#">Your Work</a>
@@ -155,10 +154,11 @@ function Home() {
               <li>
                 <a href="#">People</a>
               </li>
-              
             </ul>
+
             <button className="CreateBtn">Create</button>
           </nav>
+
           <div className="Search">
             <span class="Search-icon">&#128269;</span>
             <input
@@ -184,6 +184,33 @@ function Home() {
                             </ul>
                         </div>
                     )} */}
+        </div>
+        {/* <input type="checkbox" id="hamburger-input" className="burger-shower" /> */}
+        <div className="Hamburger-menu">
+          <div>
+            <button class="icon">☰</button>
+            
+            <div className="hamburger-dropdown">
+              
+              <ul>
+                <li>
+                  <a href="#">Your Work</a>
+                </li>
+                <li>
+                  <a href="#">Projects</a>
+                </li>
+                <li>
+                  <a href="#">Filters</a>
+                </li>
+                <li>
+                  <a href="#">Dashboard</a>
+                </li>
+                <li>
+                  <a href="#">People</a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </header>
       {/* Hier Hört der Header auf */}
